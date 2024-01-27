ee01c75feb0cb441f3c6d9b490645d15dd55317e
Derron Li <104483680+derronli@users.noreply.github.com>
2024-01-27 13:47:15 -0500

    route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "@pages/Home";
 import NotFound from "@pages/NotFound";
 import Plan from "@pages/Plan";
+import CampaignProfile from "@pages/CampaignProfile";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/plan" element={<Plan />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/profile" element={<CampaignProfile />} />
       </Routes>
     </Router>
   );
