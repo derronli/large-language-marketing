2c41d0eec9ef0c11d4353cdb164b57a2f962c028
milo <106563068+charcoalyy@users.noreply.github.com>
2024-01-27 14:11:12 -0500

    style fix

--- frontend/src/components/layouts/Dashboard.tsx
+++ frontend/src/components/layouts/Dashboard.tsx
@@ -25,9 +25,7 @@ const Dashboard = ({ children, header }: DashboardProps) => {
       <Text size="32px" sx={{ fontFamily: "Source Serif Pro" }}>
         {header}
       </Text>
-      <Flex sx={{ border: "1px solid red", width: "100%", overflow: "hidden" }}>
-        {children}
-      </Flex>
+      <Flex sx={{ width: "100%", overflow: "hidden" }}>{children}</Flex>
     </Flex>
   );
 };
