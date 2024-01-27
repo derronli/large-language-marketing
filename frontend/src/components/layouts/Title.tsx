efcb03d849a3ed1ed8826a3475139ab4ed38af9b
richardl2003 <richardliy03@gmail.com>
2024-01-27 14:37:59 -0500

    refactor: sizing on landing page

--- frontend/src/components/layouts/Title.tsx
+++ frontend/src/components/layouts/Title.tsx
@@ -29,11 +29,11 @@ const Title = ({img, mainTitle, subTitle}: TitleProps) => {
           padding: "32px",
         }}
       >
-        <Flex sx={{ width: "269px", height:"168px", top: "63px", justifyContent:"center" }}>
+        <Flex sx={{ width: "30%", height:"25%", justifyContent:"center" }}>
             <img src={img} />
         </Flex>
         <br></br>
-        <Flex sx={{ width: "774px", height: "240px", top: "266px", left: "254px"}}>
+        <Flex sx={{ width: "60%", height: "20%" }}>
           <Text size="100px" sx={{ fontFamily: "Source Serif Pro", fontWeight: 300, lineHeight: "80px", letterSpacing: "0em", textAlign: "center" }}>
             {firstHalfMain}
             <span style={{ fontStyle: "italic", fontWeight: 300 }}>deeper</span>
@@ -41,14 +41,13 @@ const Title = ({img, mainTitle, subTitle}: TitleProps) => {
           </Text>
         </Flex>
         <br></br>
-        <Flex sx={{ width: "599px", height: "75px", top: "541px", left: "340px"}}>
+        <Flex sx={{ width: "40%", height: "10%", marginTop: "5%"}}>
           <Text size="20px" fw={400} fs="italic" sx={{ fontFamily: "Source Serif Pro", textAlign: "center", lineHeight: "25px"}}>
               {subTitle}
           </Text>
         </Flex>
         
-        <Button variant="filled" color="dark" size="lg" radius="xs" sx={{marginTop: "20px", width: "213px"}} >SIGN UP</Button>
-        <Button variant="outline" color="dark" size="lg" radius="xs" sx={{marginTop: "10px", width: "213px"}} onClick={handleNavigate}>LOG IN</Button>         
+        <Button variant="filled" color="dark" size="lg" radius="xs" sx={{marginTop: "1rem", width: "15%"}} onClick={handleNavigate}>GET STARTED</Button>       
 
       </Flex>
     );
