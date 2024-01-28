286261bc0ffb8363da69caec7f68d433c5ef0682
milo <106563068+charcoalyy@users.noreply.github.com>
2024-01-27 22:59:14 -0500

    prompt tweak

--- backend/models/prompts.py
+++ backend/models/prompts.py
@@ -7,11 +7,11 @@ FIND_THEME_PROMPT="""
 """
 
 MAKE_POST_PROMPT="""
-    This social media post will be an image based on the following theme. Generate a detailed description of what the image looks like and contains. Extract only the image description itself in the response.
+    This social media post will be an image based on the following theme. Generate a description of all visual elements concerning what the image looks like and contains. Extract only the image description itself in the response. 
     Theme: {theme}
 """
 
 CAPTION_POST_PROMPT="""
-    Generate a caption for the following social media post. The caption will engage social media users who come across this post. Extract only the caption itself in the response.
+    Generate a caption for the following social media post. The caption will engage social media users who come across this post. Extract only the caption itself in the response. Remove the double quotation marks.
     Post: {post}
 """
